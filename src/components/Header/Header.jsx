b3478f0b23565e613a5c495d412d86f97c3d3b90
Jonas <jonasriedel@gmx.net>
2021-10-10 16:08:43 +0200

    Add icon to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = (props) => {
     return (
         <div>
-            <h1 className="name-heading">{props.heading}</h1>
+            <h1 className="name-heading">{props.heading} <i class="fas fa-pizza-slice"></i></h1>
             
             <h2>About me</h2>
         </div>
