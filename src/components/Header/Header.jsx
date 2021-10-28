e269c5f41af633c168bb2464dd895727ddca1363
Jonas <jonasriedel@protonmail.com>
2021-10-28 19:55:37 +0200

    Change icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import './Header.css'
 
 const Header = (props) => {
-    return (
-        <div>
-            <h1 className="name-heading">{props.heading} <i class="fas fa-pizza-slice"></i></h1>
-            
-            <h2>About me</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="name-heading">
+        {props.heading} <i class="fab fa-linux"></i>
+      </h1>
+
+      <h2>About me</h2>
+    </div>
+  )
 }
 
 export default Header
