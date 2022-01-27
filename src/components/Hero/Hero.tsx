646d8a375e8b2693b7d3522211b8d429877eaf6d
Jonas <jonasriedel@protonmail.com>
2022-01-27 23:59:25 +0100

    Remove status page

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,70 +11,34 @@ const Hero: FC = () => {
   return (
     <main>
       <p>
-        Hello, my name is{' '}
-        <Link
-          url='https://en.wikipedia.org/wiki/Jonas_(name)'
-          text='Jonas'
-          className='jonas-name-link'
-        />
-        . I am{' '}
-        <Link
-          url='https://www.rapidtables.com/convert/number/binary-to-decimal.html?x=10001'
-          text='10001'
-          className='decimal-to-binary-link'
-        />
+        Hello, my name is <Link url="https://en.wikipedia.org/wiki/Jonas_(name)" text="Jonas" className="jonas-name-link" />. I am <Link url="https://www.rapidtables.com/convert/number/binary-to-decimal.html?x=10001" text="10001" className="decimal-to-binary-link" />
         <sub>2</sub> years old.
       </p>
 
-      <img
-        className='avatar'
-        src='https://avatars.githubusercontent.com/u/76961402?v=4'
-        alt='avatar'
-      />
+      <img className="avatar" src="https://avatars.githubusercontent.com/u/76961402?v=4" alt="avatar" />
 
       <div>
-        <h1 className='communication-heading'>Communication</h1>
+        <h1 className="communication-heading">Communication</h1>
 
         <p>You can email me if you know Javascript.</p>
 
-        <div className='codeblock'>
-          <CodeBlock
-            text={code}
-            language='javascript'
-            showLineNumbers='true'
-            theme={dracula}
-          />
+        <div className="codeblock">
+          <CodeBlock text={code} language="javascript" showLineNumbers="true" theme={dracula} />
         </div>
       </div>
 
-      <p>
-        <Link
-          url='https://status.jonasriedel.com'
-          className='statusPageLink'
-          text='Status page'
-        />{' '}
-        <i className="fas fa-signal"></i>
-      </p>
-
       <h2>Socials</h2>
       <p>
-        <Link url='https://twitter.com/jvnxs7' text='jvnxs7' /> on Twitter{' '}
-        <i className='fab fa-twitter'></i>
+        <Link url="https://twitter.com/jvnxs7" text="jvnxs7" /> on Twitter <i className="fab fa-twitter"></i>
       </p>
       <p>
-        <Link url='https://github.com/jonasrdl' text='jonasrdl' /> on GitHub{' '}
-        <i className='fab fa-github'></i>
+        <Link url="https://github.com/jonasrdl" text="jonasrdl" /> on GitHub <i className="fab fa-github"></i>
       </p>
       <p>
-        <Link url='https://keybase.io/jonasrdl' text='jonasrdl' /> on Keybase{' '}
-        <i className='fab fa-keybase'></i>
+        <Link url="https://keybase.io/jonasrdl" text="jonasrdl" /> on Keybase <i className="fab fa-keybase"></i>
       </p>
       <p>
-        <Link
-          url='https://discord.com/users/209945797918195712'
-          text='jvnxs7#6969'
-        />{' '}
-        on Discord <i className='fab fa-discord'></i>
+        <Link url="https://discord.com/users/209945797918195712" text="jvnxs7#6969" /> on Discord <i className="fab fa-discord"></i>
       </p>
     </main>
   )
